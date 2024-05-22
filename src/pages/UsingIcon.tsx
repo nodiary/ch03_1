d0e5dea1556877556a890c9b3772147b3b25604c
roh <rohilkie94@gmail.com>
2024-05-22 13:45:35 +0900

    chapter3-1 - 리액트 컴포넌트의 CSS 스타일링

---
+++ src/pages/UsingIcon.tsx
@@ -0,0 +1,11 @@
+import {Icon} from '../components'
+
+export default function UsingIcon() {
+  return (
+    <div>
+      <h3>UsingIcon</h3>
+      <Icon name="home" style={{color: 'blue'}} />
+      <Icon name="home" style={{color: 'blue'}} />
+    </div>
+  )
+}
